54d9f6a21fe8e285864485c6b1260192f8c9643c
yaliu2015 <yl3268@nyu.edu>
2015-05-08 15:01:03 -0400

    remove source

--- source/getgeomerge.py
+++
@@ -1,9 +0,0 @@
-import shapefile
-
-sf = shapefile.Reader("shapefile/NYC_Census_Tract_Merge.shp")
-
-geomet = sf.shapeRecords()
-for item in geomet:
-    output_list = [item.record[0],item.shape.points]
-    print output_list
-#     print item.record # geoid
